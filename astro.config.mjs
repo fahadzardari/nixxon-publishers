53b0ea65591336db004384bf7896e69df755ff8e
Fahad Hussain Zardari <fahadzardari111@gmail.com>
2025-03-26 17:53:38 +0500

    Add initial assets, components, and configuration for the homepage layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: 'https://nixxontech.com',
+  output: 'static',
+  integrations: [react(), sitemap()],
+  devToolbar: {
+    enabled: false
+  }
+});
